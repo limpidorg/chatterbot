faa4955d0b4b52a745785035a0c7eb6e94bf843c
yyjlincoln <yyjlincoln@gmail.com>
2021-12-18 23:54:16 +1100

    Update: Commit changes

--- bot.py
+++ bot.py
@@ -9,47 +9,6 @@ intents.members = True
 
 bot = commands.Bot(intents=intents, command_prefix="!")
 
-testUser = "yyjlincoln#5912"
-# testUser = "Bonsaï#8521"
-
-
-@bot.event
-async def on_ready():
-    print("------")
-    print("Logged in as")
-    print(bot.user.name)
-    print(bot.user.id)
-    print("------")
-
-    dispatchSendMessageEvent(
-        testUser,
-        {
-            "title": f"Hello {testUser}",
-            "description": "Here is the reason of my message.",
-            "advices": [
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-            ],
-        },
-    )
-
-    await bot.change_presence(activity=discord.Game(name="conveying happiness ;)"))
-
-
 @bot.event
 async def on_send_message(username, message):
     members = {}
@@ -83,5 +42,3 @@ async def on_send_message(username, message):
 def dispatchSendMessageEvent(username, message):
     bot.dispatch("send_message", username, message)
 
-
-bot.run("OTIxNjgxOTg0MDM0NTk4OTIz.Yb2dPQ.xb1BnBZ5TkmOkJyT40qGtXmN-dk")
