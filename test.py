c390324ea3c685f9404843c9c6be86e6f8eb2ae0
EsprIx <46829058+EsprIx@users.noreply.github.com>
2021-12-19 07:46:20 +0100

    v2

--- test.py
+++ test.py
@@ -1,32 +1,25 @@
-from time import sleep
-from bot import dispatchSendMessageEvent
+import asyncio
+import threading
 
-testUser = "Bonsaï#8521"
+from run import bot, t_run
 
-sleep(5)
-print("go")
-dispatchSendMessageEvent(
-        testUser,
-        {
-            "title": f"Hello {testUser}",
-            "description": "Here is the reason of my message.",
-            "advices": [
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-                {
-                    "title": "Don't be rude!",
-                    "description": "The person you are talking to might be going through hard times...",
-                },
-            ],
-        },
-)
+loop = asyncio.new_event_loop()
+t = threading.Event()
+
+
+def hasJoinedDiscord(username):
+    loop.call_soon_threadsafe(asyncio.ensure_future, bot.has_joined_discord(username, t))
+    t.wait(5)
+    res = t.has_joined_discord
+    print(res)
+    return res
+
+
+def sendMessage(id, message):
+    asyncio.ensure_future(bot.send_message(id, message))
+
+
+id = hasJoinedDiscord("Bonsaï#8521")
+sendMessage(id, "hi there im so brilliant")
+
+loop.call_soon_threadsafe(loop.stop)
